f56d53791c8bdf7cf2c47d20ec646faee038d85e
Wix Remote Machine <support@wix.com>
2026-04-21 21:25:39 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -76,7 +76,7 @@ export default function HomePage() {
       {/* HERO */}
       <section className="min-h-screen w-full text-white relative overflow-hidden">
         <header className="absolute w-full flex justify-between px-6 md:px-10 py-4 z-50 items-center">
-          <Image src="https://static.wixstatic.com/media/792b6b_db67302a203f42429486f97568002b88~mv2.png?originWidth=1152&originHeight=576" alt="Logo Jeri4xp" className="w-20 md:w-24" />
+          <Image src="./assets/kite" alt="Logo Jeri4xp" className="w-20 md:w-24" />
 
           <nav className="hidden md:flex gap-8 text-sm font-bold uppercase tracking-wider">
             {navItems.map((item) => (
