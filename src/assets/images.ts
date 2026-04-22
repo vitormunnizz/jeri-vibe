4f53d3e0b02eb0ad49489038a8705c0f8e03e7e3
Wix Remote Machine <support@wix.com>
2026-04-22 23:53:35 +0000

    Atualizando caminhos de imagem

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,37 +1,39 @@
 // Image exports for the Jeri4xp website
 // All images are centralized here for easy management
 
+const kitePreaPath = '../src/assets/kitePrea.png';
+
 export const images = {
   // Logo
-  logo: './assets/kitePrea.png',
+  logo: kitePreaPath,
 
   // Hero backgrounds
-  heroHome: 'https://static.wixstatic.com/media/792b6b_3bd32de2fa6b4a9496517e7ce9c1b3d7~mv2.png',
-  heroPasseios: 'https://static.wixstatic.com/media/792b6b_f1a29dfb5f294176b121771875f4bafa~mv2.png?originWidth=1152&originHeight=576',
-  heroHospedagem: '/jeri1.webp',
-  heroTransfer: 'https://static.wixstatic.com/media/792b6b_c183b922ef4d44088189659bc81a1704~mv2.png',
-  heroAulas: 'https://static.wixstatic.com/media/792b6b_6fccfcc639634ffb866c533270bda3b9~mv2.png',
+  heroHome: kitePreaPath,
+  heroPasseios: kitePreaPath,
+  heroHospedagem: kitePreaPath,
+  heroTransfer: kitePreaPath,
+  heroAulas: kitePreaPath,
 
   // Experience cards
-  experiencePasseios: 'https://static.wixstatic.com/media/792b6b_4f20f630624849c0af228c95edd905b7~mv2.png?originWidth=256&originHeight=256',
-  experienceHospedagem: 'https://static.wixstatic.com/media/792b6b_ea2817bcd0de4f6e8d235353decd6caf~mv2.png?originWidth=256&originHeight=256',
-  experienceTransfer: 'https://static.wixstatic.com/media/792b6b_24b85beb673d4e1bbd945bb5e3412cd4~mv2.png?originWidth=256&originHeight=256',
-  experienceAulas: 'https://static.wixstatic.com/media/792b6b_308bfaa4612a43949f0b088dfb2b0b83~mv2.png?originWidth=256&originHeight=256',
+  experiencePasseios: kitePreaPath,
+  experienceHospedagem: kitePreaPath,
+  experienceTransfer: kitePreaPath,
+  experienceAulas: kitePreaPath,
 
   // Carousel images
-  carouselPasseios: 'https://static.wixstatic.com/media/792b6b_d4c5932b099c4552a2356fa26ce00f01~mv2.png?originWidth=576&originHeight=384',
-  carouselHospedagem: '/logo_jeri4xp.png',
+  carouselPasseios: kitePreaPath,
+  carouselHospedagem: kitePreaPath,
 
   // Transfer images
-  transferFortaleza: 'https://static.wixstatic.com/media/792b6b_d4c5932b099c4552a2356fa26ce00f01~mv2.png?originWidth=576&originHeight=384',
-  transferCruz: 'https://static.wixstatic.com/media/792b6b_d4c5932b099c4552a2356fa26ce00f01~mv2.png?originWidth=576&originHeight=384',
+  transferFortaleza: kitePreaPath,
+  transferCruz: kitePreaPath,
 
   // Aulas images
-  aulasKitesurf: 'https://static.wixstatic.com/media/792b6b_8422e221a25f4714825deac834453cb3~mv2.png?originWidth=768&originHeight=448',
-  aulasWindsurf: 'https://static.wixstatic.com/media/792b6b_6c8eafd808e4465f8481472f77bfb76e~mv2.png?originWidth=768&originHeight=448',
-  aulasSurf: 'https://static.wixstatic.com/media/792b6b_2fab59f071e54e1ba72c8513563d5e41~mv2.png?originWidth=768&originHeight=448',
-  aulasWingFoil: 'https://static.wixstatic.com/media/792b6b_54cbbf0d83184de8904ab7160600cc23~mv2.png?originWidth=768&originHeight=448',
+  aulasKitesurf: kitePreaPath,
+  aulasWindsurf: kitePreaPath,
+  aulasSurf: kitePreaPath,
+  aulasWingFoil: kitePreaPath,
 
   // Avatar placeholders
-  avatarPlaceholder: (seed: string) => `https://i.pravatar.cc/150?u=${seed}`,
+  avatarPlaceholder: (seed: string) => kitePreaPath,
 };
