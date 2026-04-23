c890e401ac0371bca1f70ec13d4abe1af4f6202d
Wix Remote Machine <support@wix.com>
2026-04-23 16:08:46 +0000

    Manual code changes

--- src/components/pages/TransferPage.tsx
+++ src/components/pages/TransferPage.tsx
@@ -85,7 +85,7 @@ export default function TransferPage() {
 
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6">
 
-          <h1 className="text-3xl md:text-5xl font-black italic tracking-tighter mb-6">
+          <h1 className="text-3xl md:text-5xl font-black italic tracking-tighter mb-6 text-shadow-md">
             Como Chegar em Jericoacoara com <br /> Transfer Seguro e Confortável
           </h1>
 
